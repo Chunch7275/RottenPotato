9236a4bdfc217e4960d968d321cc739053d70199
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2025-04-30 14:27:31 -0700

    translucent obstacles
    obstacles turn translucent when they are hovered over

--- RottenPotatoes/Assets/Scripts/Obstacle.cs
+++ RottenPotatoes/Assets/Scripts/Obstacle.cs
@@ -3,9 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
+
 {
+    [SerializeField] private Material hoverMaterial;    
+    private Material originalMaterial;                   
+    private Renderer objRenderer;
+
     // Collisions destroy the grid that is made by the pathfinding. 
     // Set this on obstacles, make sure they are rigidbodies and kinematic.
+
+    private void Awake()
+    {
+        objRenderer = GetComponent<Renderer>();
+        if (objRenderer != null)
+        {
+            originalMaterial = objRenderer.material;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (objRenderer != null && hoverMaterial != null)
+        {
+            objRenderer.material = hoverMaterial;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (objRenderer != null && originalMaterial != null)
+        {
+            objRenderer.material = originalMaterial;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,4 +55,3 @@ public class Obstacle : MonoBehaviour
         }
     }
 }
-

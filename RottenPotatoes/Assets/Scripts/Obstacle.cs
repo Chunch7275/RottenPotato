6f97746ece10fbcc29f5355ac1c24d1960ed92ab
San Diego Mesa College <sdmesasoftware@sdccd.edu>
2025-05-01 12:04:26 -0700

    Prepped Trees for being made Translucent

--- RottenPotatoes/Assets/Scripts/Obstacle.cs
+++ RottenPotatoes/Assets/Scripts/Obstacle.cs
@@ -25,6 +25,7 @@ public class Obstacle : MonoBehaviour
     {
         if (objRenderer != null && hoverMaterial != null)
         {
+            //We'll need to set the Tree Prefab's material's Alphas to a lower setting when this fires.  That should allow the trees to become translucent when hovering over them.  -5/1/25 Ben Heifetz
             objRenderer.material = hoverMaterial;
         }
     }

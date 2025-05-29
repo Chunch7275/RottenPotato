ea48a46fc104467ca89f2f1a7011b08604283250
Chunch <NWAColec7275@gmail.com>
2025-05-28 23:50:03 -0700

    house to mainmenu

--- RottenPotatoes/Assets/Scripts/Enemey/EnemyHealth.cs
+++ RottenPotatoes/Assets/Scripts/Enemey/EnemyHealth.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 10;
     public int currentHealth { get; private set; }
+
+    [Tooltip("Name of the scene to load when this enemy dies.")]
+    public string sceneToLoadOnDeath;
 
     void Start()
     {
@@ -23,6 +27,17 @@ public class EnemyHealth : MonoBehaviour
 
     private void Die()
     {
+        // Optional: Destroy the enemy before loading scene, or comment this out if you don't want that
         Destroy(gameObject);
+
+        // Load the specified scene
+        if (!string.IsNullOrEmpty(sceneToLoadOnDeath))
+        {
+            SceneManager.LoadScene(sceneToLoadOnDeath);
+        }
+        else
+        {
+            Debug.LogWarning("[EnemyHealth] No scene name specified to load on death.");
+        }
     }
 }
